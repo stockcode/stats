b649769a13c655a335ae6a56ef0f43284f27887a
stockcode <stockcode@sina.cn>
2012-11-12 16:52:17 +0800

    首次提交

---
+++ stats.dpr
@@ -0,0 +1,13 @@
+program stats;
+
+uses
+  Forms,
+  u_main in 'u_main.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
